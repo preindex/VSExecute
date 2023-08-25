b96690dc1beb96c76a5cb61ede6a7805e4725ce4
preindex <48609126+preindex@users.noreply.github.com>
2023-08-25 06:40:46 -0700

    Create extension

---
+++ extension.js
@@ -0,0 +1,59 @@
+const Port = 13370
+
+const vs = require('vscode')
+const ws = require('ws')
+
+const Output = vs.window.createOutputChannel("Execution Output")
+Output.show()
+
+const Server = new ws.Server({port: Port})
+console.log(`Created Server @ Port ${Port}`)
+
+let Connections = []
+
+function Init(Socket) {
+	Connections.push(Socket)
+	Socket.on("message", Message => {
+		let Data = JSON.parse(Message)
+		Output.appendLine(`[${Data.Type} - ${Data.User}]: ${Data.Output}`)
+	})
+}
+
+function activate(context) {
+	console.log('VSExecute has been activated.');
+
+	Server.on("connection", Init)
+
+	context.subscriptions.push(vs.commands.registerCommand('vsexecute.execute', () => {
+		// Execute
+		let Editor = vs.window.activeTextEditor
+		if (Editor) {
+			for (let Connection in Connections) {
+				Connections[Connection].send(JSON.stringify({type: "script", script: Editor.document.getText()}))
+			}
+		}
+	}))
+
+	if (Item) return;
+
+	Item = vs.window.createStatusBarItem(vs.StatusBarAlignment.Left)
+	Item.text = "$(debug-start) Execute"
+	Item.tooltip = "Execute Script"
+	Item.command = "vsexecute.execute"
+	Item.show()
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {
+	Server.close()
+	for (let Connection in Connections) {
+		Connections[Connection].terminate()
+	}
+	Item.hide()
+	Item.dispose()
+}
+
+module.exports = {
+	activate,
+	deactivate
+}
