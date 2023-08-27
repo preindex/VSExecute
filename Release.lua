048c38275814cbfe132320f99b6e82fbaf0a3dfd
preindex <48609126+preindex@users.noreply.github.com>
2023-08-26 22:50:30 -0700

    Wrong link

--- Release.lua
+++ Release.lua
@@ -1,7 +1,7 @@
 local File = isfile("WebSocketClient.lua")
 
 if not File then
-    writefile("WebSocketClient.lua", game:HttpGet("https://scripts.system-exodus.com/assets/VSExecute.lua"))
+    writefile("WebSocketClient.lua", game:HttpGet("https://scripts.system-exodus.com/assets/VSExecute/Client.lua"))
 end
 
 loadstring(readfile("WebSocketClient.lua"))()
